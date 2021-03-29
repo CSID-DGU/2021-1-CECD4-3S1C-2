3f0863bee1f5923e1a58bc2c129984170f6a3a09
LuneRemer <europejumin@gmail.com>
2021-03-29 13:48:34 +0900

    # of comments bug fixed

--- news_politic.py
+++ news_politic.py
@@ -28,15 +28,12 @@ texts_article = []
 texts_comment = []
 texts_title = []
 texts_url = []
-rreal_url = []
-choreal_url = []
 
 commentss = ''
 
 
 def crawling_url(url):
-    global rreal_url
-    global choreal_url
+    global texts_url
     session = HTMLSession()
     res = session.get(url)
 
@@ -50,12 +47,13 @@ def crawling_url(url):
                 check = real_url[z]['href']
                 if 'https://news.naver.com' in check:
                     if 'sid1=100' in check:
-                        rreal_url.append(check)
-    rreal_url = set(rreal_url)
+                        texts_url.append(check)
+    texts_url = set(texts_url)
+    texts_url = list(texts_url)
 
-    print("******")
-    print(rreal_url)
-    print("*******")
+    # print("******")
+    # print(texts_url)
+    # print("*******")
     # if url:
     #     global texts_url
 
@@ -93,6 +91,7 @@ crawling_url('https://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=100')
 
 print("*********url 정보***********")
 print(texts_url)
+print(len(texts_url))
 
 oid = []
 aid = []
@@ -107,33 +106,43 @@ for y in range(len(texts_url)):
 
 useragent = UserAgent()
 
-for v in range(len(texts_url)):
-    headers.append({
-        'referer': texts_url[v],
-        'User-Agent': useragent.chrome
-    })
 for x in range(len(texts_url)):
-    com_url.append("https://apis.naver.com/commentBox/cbox/web_neo_list_jsonp.json?ticket=news&templateId=default_society&pool=cbox5&_callback=jQuery1707138182064460843_1523512042464&lang=ko&country=&objectId=news" + oid[x] + "%2C" + aid[x] + "&categoryId=&pageSize=20&indexSize=10&groupId=&listType=OBJECT&pageType=more&page=" + str(
-        page) + "&refresh=false&sort=FAVORITE")
+    comps_url = []
+    for y in range(1, 10):
+        comps_url.append("https://apis.naver.com/commentBox/cbox/web_neo_list_jsonp.json?ticket=news&templateId=default_politics&pool=cbox5&_callback=jQuery1707138182064460843_1523512042464&lang=ko&country=&objectId=news" +
+                         oid[x] + "%2C" + aid[x] + "&categoryId=&pageSize=20&indexSize=10&groupId=&listType=OBJECT&pageType=more&page=" + str(y) + "&refresh=false&sort=FAVORITE")
+        com_url.append(comps_url)
 
-for y in range(len(texts_url)):
-    resp = urlopen(
-        Request(com_url[y], headers=headers[y])).read().decode('utf-8')
-    x = 0
-    while(resp[x] != '{'):
-        x += 1
-    # print(response[x:])
+for v in range(len(texts_url)):
+    headerL = []
+    for z in range(len(com_url)):
+        headerL.append({
+            'referer': texts_url[v],
+            'User-Agent': useragent.chrome
+        })
+        headers.append(headerL)
 
-    z = len(resp)-1
-    while(resp[z] != '}'):
-        z -= 1
-    # print(response[x:y]+'}')
+for x in range(len(texts_url)):
+    for y in range(len(com_url[x])):
+        resp = urlopen(
+            Request(com_url[x][y], headers=headers[x][y])).read().decode('utf-8')
+        if resp:
+            x = 0
+            while(resp[x] != '{'):
+                x += 1
+            # print(response[x:])
 
-    rank_json = json.loads(resp[x:z]+'}')
-    for w in range(len(rank_json['result']['commentList'])):
-        texts_comment.append(rank_json['result']['commentList'][w]['contents'])
-        commentss = commentss + \
-            rank_json['result']['commentList'][w]['contents']
+            z = len(resp)-1
+            while(resp[z] != '}'):
+                z -= 1
+            # print(response[x:y]+'}')
+
+            rank_json = json.loads(resp[x:z]+'}')
+            for w in range(len(rank_json['result']['commentList'])):
+                texts_comment.append(
+                    rank_json['result']['commentList'][w]['contents'])
+                commentss = commentss + \
+                    rank_json['result']['commentList'][w]['contents']
 
 for y in range(len(texts_url)):
     crawling_article(texts_url[y])
@@ -141,7 +150,8 @@ for y in range(len(texts_url)):
 
 print("**********title************")
 print(texts_title)
-print("**********article**********")
-print(texts_article)
+# print("**********article**********")
+# print(texts_article)
 print("**********comments**********")
 print(texts_comment)
+print(len(texts_comment))
