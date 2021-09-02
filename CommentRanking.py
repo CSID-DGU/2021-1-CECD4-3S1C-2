cc5094de182c26926eee0ee501b8ae6325d5ab43
DevYJKwon <dydwo2324@gmail.com>
2021-09-02 20:09:55 +0900

    ver 1.0.1

--- CommentRanking.py
+++ CommentRanking.py
@@ -2,20 +2,22 @@ from konlpy.tag import Okt
 from collections import Counter
 
 #파일 경로 설정
-filename = ' '
+filename = 'comment_collection.csv'
 f= open(filename,'r',encoding='utf-8')
 news = f.read()
 
 #댓글 중 명사만 추출
 okt=Okt()
 noun = okt.nouns(news)
-for i,item in enumerate(noun):
+
+for item in noun[:]:
     if len(item)<2: #명사의 길이가 2이상인 것만 추출 
-        noun.pop(i)
+        noun.remove(item)
+
 
 count = Counter(noun)
 f.close()
 
-noun_list = count.most_common(1000) #most_common() : 매게변수 개수만큼 등수 추출
+noun_list = count.most_common(10) #most_common() : 매개변수 개수만큼 등수 추출
 for list in noun_list:
     print(list)
