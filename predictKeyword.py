b14246023513e7df69eddcdf6294ac398c0a1ed7
DevYJKwon <dydwo2324@gmail.com>
2021-10-07 15:19:08 +0900

    ver 1.1

--- predictKeyword.py
+++ predictKeyword.py
@@ -1,29 +1,33 @@
+from os import fdopen
 import comment_model_simple as model 
 from TextManager import TextManager
 
+filename='./test.csv'
 text=TextManager()
-temp=text.ExtractKeyword(30,'./comment_collection.csv')
+temp=text.ExtractKeyword(10,filename)
 keyword=[]
 for i in temp:
     keyword.append(i[0])
-commentList=text.ExtractComments(keyword,'./comment_collection.csv')
 
+print(keyword)
+commentList=text.ExtractComments(keyword,filename)
+
+filename2='./result.txt'
+f= open(filename2,'w')
 
 for line in commentList:
-    score=0.5
+    score=0
     for i in range(1,len(line)):
         predict=model.sentiment_predict(line[i])
-        if predict >0.5:
-            score=score+(predict-0.5)
-        elif predict == 0.5:
-            pass
-        else:
-            score=score-(0.5-predict)
+        f.write(str(predict)+'\n')
+        score=score+predict
     score=score/len(line)
 
-    if score >0.6:
+    if score >0.4:
         print(line[0],'긍정 키워드입니다.',score)
-    elif score >= 0.4:
+    elif score >= 0.30103:
         print(line[0],'중립 키워드입니다.',score)
     else:
         print(line[0],'부정 키워드입니다.',score)
+
+f.close()
