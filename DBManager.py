0cb7abdf46e2e4e8b03f7aa2cd61b899ad58c578
dfdf <rkrpwjd112233@gmail.com>
2021-11-02 14:40:29 +0900

    ver 1.6 (add devAlgorithm)

--- DBManager.py
+++ DBManager.py
@@ -11,31 +11,53 @@ class DB:
             charset="utf8"
         )
         self.cursor=self.db.cursor()
-
+    def saveDB(self,news):
+        pass
+    def checkEmpty(self):
+        pass
 class KeywordDB(DB):
 
     def saveDB(self,news):
+        self.checkEmpty()
         keywordList= news.getKeywordList()
         valueList= news.getValueList()
         for i in range(0,len(keywordList)):
             keyword=keywordList[i][0]
             ranks=i+1
             mentions=keywordList[i][1]
-            if valueList[i] < 0.5:
+            if valueList[i] < 0.30103:
                 value=(1-valueList[i])*100
                 neg= round(value,2)
                 pos= 100-neg
             else:
                 value=valueList[i]*100
                 pos= round(value,2)
-                neg= 100-pos   
-            
-            sql="REPLACE INTO keywords (keyword, positive, negative, ranks, mentions) values (%s, %s, %s, %s, %s)"
-            self.cursor.execute(sql, (keyword, pos, neg, ranks, mentions))    
+                neg= 100-pos  
+            sql="REPLACE INTO devkeywords (keyword, positive, negative, ranks, mentions,id) values (%s, %s, %s, %s, %s,%s)"
+            self.cursor.execute(sql, (keyword, pos, neg, ranks, mentions, 0))    
             self.db.commit()
+
+        sql="DELETE FROM keywords"
+        self.cursor.execute(sql)
+        self.db.commit()
+        sql="INSERT INTO keywords SELECT * FROM devkeywords"
+        self.cursor.execute(sql)
+        self.db.commit()
+        
+        
+    def checkEmpty(self):
+        sql="select EXISTS (select * from devkeywords limit 1) as success"
+        self.cursor.execute(sql)
+        self.db.commit()
+        result=self.cursor.fetchone()
+        if result:
+            sql="DELETE FROM devkeywords"
+            self.cursor.execute(sql)
+            self.db.commit()
+
     
     def FetchData(self):
-        sql="SELECT contents FROM comments" 
+        sql="SELECT contents FROM comments WHERE news_id=2" 
         self.cursor.execute(sql)
         self.db.commit()
         temp=self.cursor.fetchall()
@@ -52,7 +74,9 @@ class KeywordDB(DB):
         return data
         
 class RelDB(DB):
+
     def saveDB(self,news):
+        self.checkEmpty()
         keywordList= news.getKeywordList()
         relKeywordList=news.getRelList()
         for i in range(0,len(keywordList)):
@@ -60,6 +84,23 @@ class RelDB(DB):
             relkeyword=relKeywordList[i]
             for j in range(0,len(relkeyword)):
                 tuple=(*relkeyword[j],keyword)
-                sql="REPLACE INTO relkeywords (content, mentions, keyword) values (%s, %s, %s)"
+                sql="REPLACE INTO devrelkeywords (content, mentions, keyword) values (%s, %s, %s)"
                 self.cursor.execute(sql, tuple)
                 self.db.commit()
+                
+        sql="DELETE FROM relkeywords"
+        self.cursor.execute(sql)
+        self.db.commit()
+        sql="INSERT INTO relkeywords SELECT * FROM devrelkeywords"
+        self.cursor.execute(sql)
+        self.db.commit()
+    
+    def checkEmpty(self):
+        sql="select EXISTS (select * from devrelkeywords limit 1) as success"
+        self.cursor.execute(sql)
+        self.db.commit()
+        result=self.cursor.fetchone()
+        if result:
+            sql="DELETE FROM devrelkeywords"
+            self.cursor.execute(sql)
+            self.db.commit()
